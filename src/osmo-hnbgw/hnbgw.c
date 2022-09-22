osmocom/osmo-hnbgw

commit 1de2091515530e531196ebca27b1445cecd1b88d

hnbgw: Unregister HNB if SCTP link is restarted
Sometimes an hNodeB may reconnect (SCTP INIT) using same SCTP tuple without
closing the previous conn. This is handled by the SCTP stack by means of
pushing a RESET notification up the stack to the sctp_recvmsg() user.
Let's handle this by marking the HNB as unregistered, since most
probably a HNB Register Req comes next as the upper layer state is
considered lost.

Depends: libosmo-netif.git Change-Id I0ee94846a15a23950b9d70eaaef1251267296bdd
Related: SYS#6113
Change-Id: Ib22881b1a34b1c3dd350912b3de8904917cf34ef

src/osmo-hnbgw/hnbgw.c
@@ -251,8 +251,25 @@ static int hnb_read_cb(struct osmo_stream_srv *conn)
 	msg->dst = hnb;
 
 	rc = osmo_stream_srv_recv(conn, msg);
-	if (rc == -EAGAIN) {
-		/* Notification received */
+	/* Notification received */
+	if (msgb_sctp_msg_flags(msg) & OSMO_STREAM_SCTP_MSG_FLAGS_NOTIFICATION) {
+		union sctp_notification *notif = (union sctp_notification *)msgb_data(msg);
+		switch (notif->sn_header.sn_type) {
+		case SCTP_ASSOC_CHANGE:
+			switch (notif->sn_assoc_change.sac_state) {
+			case SCTP_RESTART:
+				LOGHNB(hnb, DMAIN, LOGL_NOTICE, "HNB SCTP conn RESTARTed, marking as HNBAP-unregistered\n");
+				hnb->hnb_registered = false;
+				break;
+			}
+			break;
+		}
+		msgb_free(msg);
+		return 0;
+	} else if (rc == -EAGAIN) {
+		/* Older versions of osmo_stream_srv_recv() not supporting
+		 * msgb_sctp_msg_flags() may still return -EAGAIN when an sctp
+		 * notification is received. */
 		msgb_free(msg);
 		return 0;
 	} else if (rc < 0) {

commit 3bf5395102806e97f2c0e750c8adcc75816a3b42

hnbgw: Fix recent regression not closing conn upon rx of SCTP_SHUTDOWN_EVENT
Before handling of OSMO_STREAM_SCTP_MSG_FLAGS_NOTIFICATION in recent
commit (see Fixes: below), osmo_stream_srv_recv() and
internal _sctp_recvmsg_wrapper() in libosmo-netif would return either
-EAGAIN or 0 when an sctp notification was received from the kernel.

After adding handling of OSMO_STREAM_SCTP_MSG_FLAGS_NOTIFICATION, the
code paths for "rc == -EAGAIN" and "rc == 0" would not be executed
anymore since the first branch takes preference in the if-else
tree. For "rc == -EAGAIN" it's fine because the new branch superseeds
what's done on the "rc == -EAGAIN" branch. However, for the "rc == 0",
we forgot to actually destroy the connection. The "rc == 0" branch was
basically reached when SCTP_SHUTDOWN_EVENT was received because
osmo_stream_srv_recv() tried to resemble the interface of regular
recv(); let's hence check for that explicitly and destroy the conn
object (and the related hnb context in the process) when we receive
that event.

Fixes: 1de2091515530e531196ebca27b1445cecd1b88d
Related: SYS#6113
Change-Id: I11b6af51a58dc250975a696b98d0c0c9ff3df9e0

src/osmo-hnbgw/hnbgw.c
@@ -254,6 +254,7 @@ static int hnb_read_cb(struct osmo_stream_srv *conn)
 	/* Notification received */
 	if (msgb_sctp_msg_flags(msg) & OSMO_STREAM_SCTP_MSG_FLAGS_NOTIFICATION) {
 		union sctp_notification *notif = (union sctp_notification *)msgb_data(msg);
+		rc = 0;
 		switch (notif->sn_header.sn_type) {
 		case SCTP_ASSOC_CHANGE:
 			switch (notif->sn_assoc_change.sac_state) {
@@ -261,17 +262,23 @@ static int hnb_read_cb(struct osmo_stream_srv *conn)
 				LOGHNB(hnb, DMAIN, LOGL_NOTICE, "HNB SCTP conn RESTARTed, marking as HNBAP-unregistered\n");
 				hnb->hnb_registered = false;
 				break;
+			case SCTP_SHUTDOWN_EVENT:
+				LOGHNB(hnb, DMAIN, LOGL_NOTICE,
+				       "sctp_recvmsg(%s) = SCTP_SHUTDOWN_EVENT, closing conn\n",
+				       osmo_sock_get_name2(osmo_stream_srv_get_ofd(conn)->fd));
+				osmo_stream_srv_destroy(conn);
+				rc = -1;
+				break;
 			}
 			break;
 		}
-		msgb_free(msg);
-		return 0;
+		goto out;
 	} else if (rc == -EAGAIN) {
 		/* Older versions of osmo_stream_srv_recv() not supporting
 		 * msgb_sctp_msg_flags() may still return -EAGAIN when an sctp
 		 * notification is received. */
-		msgb_free(msg);
-		return 0;
+		rc = 0;
+		goto out;
 	} else if (rc < 0) {
 		LOGHNB(hnb, DMAIN, LOGL_ERROR, "Error during sctp_recvmsg(%s)\n",
 		       osmo_sock_get_name2(osmo_stream_srv_get_ofd(conn)->fd));