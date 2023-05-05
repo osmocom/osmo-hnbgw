83dce9f7dacb3b0e160eb566bd49b7a41f8214f4
Neels Janosch Hofmeyr <nhofmeyr@sysmocom.de>
2023-05-05 04:18:26 +0200

    add hnbgw_decode_ranap_co()
    An upcoming patch will re-use this pattern of decoding RANAP and
    attaching a talloc destructor to the result for the third time. The
    caller will be in hnbgw_rua.c, so make this a "public" function.
    
    Change-Id: I3695babfb083ed1f5fff700dcb7646fa95496a52

--- include/osmocom/hnbgw/hnbgw_rua.h
+++ include/osmocom/hnbgw/hnbgw_rua.h
@@ -2,6 +2,7 @@
 
 #include <osmocom/hnbgw/hnbgw.h>
 #include <osmocom/rua/RUA_Cause.h>
+#include <osmocom/ranap/ranap_ies_defs.h>
 
 int hnbgw_rua_rx(struct hnb_context *hnb, struct msgb *msg);
 
@@ -10,3 +11,5 @@ int rua_tx_dt(struct hnb_context *hnb, int is_ps, uint32_t context_id,
 	      const uint8_t *data, unsigned int len);
 int rua_tx_disc(struct hnb_context *hnb, int is_ps, uint32_t context_id,
 	        const RUA_Cause_t *cause, const uint8_t *data, unsigned int len);
+
+ranap_message *hnbgw_decode_ranap_co(struct msgb *ranap_msg);
