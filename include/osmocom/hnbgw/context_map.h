b1c0bb19e2bfbc48e05a89a5f09b476d1243b5f3
Neels Janosch Hofmeyr <nhofmeyr@sysmocom.de>
2023-05-17 02:05:31 +0200

    cnpool: add context_map_cnlink_lost() handling
    When proper RANAP RESET handling is in place [1], a specific CN link may
    be detected to be disconnected or reconnected at any point.
    [1] Id3eefdea889a736fd5957b80280fa45b9547b792
    
    When that happens, all related context maps should disconnect RUA and
    SCCP links immediately. Add the context map side of this, so that
    context_map_cnlink_lost() is ready to be dispatched in [1].
    
    Related: SYS#6412
    Change-Id: Ic0a6fcfb747dc093528ca2bd12a269ad390d465c

--- include/osmocom/hnbgw/context_map.h
+++ include/osmocom/hnbgw/context_map.h
@@ -66,6 +66,9 @@ enum map_sccp_fsm_event {
 	/* The human admin asks to drop the current SCCP connection, by telnet VTY 'apply sccp' in presence of SCCP
 	 * config changes. */
 	MAP_SCCP_EV_USER_ABORT,
+	/* The CN link can no longer work, for example a RANAP RESET was received from the cnlink that hosts this
+	 * context map. */
+	MAP_SCCP_EV_CN_LINK_LOST,
 };
 
 /* For context_map_get_state(), to combine the RUA and SCCP states, for VTY reporting only. */
@@ -186,6 +189,7 @@ int _map_sccp_dispatch(struct hnbgw_context_map *map, uint32_t event, struct msg
 
 bool map_rua_is_active(struct hnbgw_context_map *map);
 bool map_sccp_is_active(struct hnbgw_context_map *map);
+void context_map_cnlink_lost(struct hnbgw_context_map *map);
 void context_map_free(struct hnbgw_context_map *map);
 
 unsigned int msg_has_l2_data(const struct msgb *msg);
