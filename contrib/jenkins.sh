fb450888b6d7fb5a3501b52129ba9a8c864ccfc3
Pau Espin Pedrol <pespin@sysmocom.de>
2024-11-21 15:14:21 +0100

    jenkins.sh: libosmo-netif no longer depends on libosmo-abis
    Depends: libosmo-abis.git Change-Id I079dc3999de508301dd37ed03e399356a58d3cab
    Depends: libosmo-netif.git Change-Id I13d6e88158f6d9ce017986283183ee9c2cc68cae
    Change-Id: I27a938f2a178a5ff59f371ec4d19b4405737eb51

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -34,8 +34,8 @@ export LD_LIBRARY_PATH="$inst/lib"
 export PATH="$inst/bin:$PATH"
 
 osmo-build-dep.sh libosmocore "" --disable-doxygen
+osmo-build-dep.sh libosmo-netif "" --disable-doxygen
 osmo-build-dep.sh libosmo-abis
-osmo-build-dep.sh libosmo-netif
 osmo-build-dep.sh libosmo-sigtran
 osmo-build-dep.sh libasn1c
 osmo-build-dep.sh osmo-iuh
