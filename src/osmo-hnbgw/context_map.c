osmocom/osmo-hnbgw

commit b1c0bb19e2bfbc48e05a89a5f09b476d1243b5f3

cnpool: add context_map_cnlink_lost() handling
When proper RANAP RESET handling is in place [1], a specific CN link may
be detected to be disconnected or reconnected at any point.
[1] Id3eefdea889a736fd5957b80280fa45b9547b792

When that happens, all related context maps should disconnect RUA and
SCCP links immediately. Add the context map side of this, so that
context_map_cnlink_lost() is ready to be dispatched in [1].

Related: SYS#6412
Change-Id: Ic0a6fcfb747dc093528ca2bd12a269ad390d465c

src/osmo-hnbgw/context_map.c
@@ -183,6 +183,11 @@ void context_map_hnb_released(struct hnbgw_context_map *map)
 	map_rua_dispatch(map, MAP_RUA_EV_HNB_LINK_LOST, NULL);
 }
 
+void context_map_cnlink_lost(struct hnbgw_context_map *map)
+{
+	map_sccp_dispatch(map, MAP_SCCP_EV_RAN_LINK_LOST, NULL);
+}
+
 void context_map_free(struct hnbgw_context_map *map)
 {
 	/* guard against FSM termination infinitely looping back here */

src/osmo-hnbgw/context_map_sccp.c
@@ -55,6 +55,7 @@ static const struct value_string map_sccp_fsm_event_names[] = {
 	OSMO_VALUE_STRING(MAP_SCCP_EV_RAN_LINK_LOST),
 	OSMO_VALUE_STRING(MAP_SCCP_EV_RX_RELEASED),
 	OSMO_VALUE_STRING(MAP_SCCP_EV_USER_ABORT),
+	OSMO_VALUE_STRING(MAP_SCCP_EV_CN_LINK_LOST),
 	{}
 };
 
@@ -284,6 +285,7 @@ static void map_sccp_init_action(struct osmo_fsm_inst *fi, uint32_t event, void 
 	case MAP_SCCP_EV_RAN_LINK_LOST:
 	case MAP_SCCP_EV_RAN_DISC:
 	case MAP_SCCP_EV_USER_ABORT:
+	case MAP_SCCP_EV_CN_LINK_LOST:
 		/* No CR has been sent yet, just go to disconnected state. */
 		if (msg_has_l2_data(ranap_msg))
 			LOG_MAP(map, DLSCCP, LOGL_ERROR, "SCCP not connected, cannot dispatch RANAP message\n");
@@ -321,6 +323,7 @@ static void map_sccp_wait_cc_action(struct osmo_fsm_inst *fi, uint32_t event, vo
 	case MAP_SCCP_EV_RAN_LINK_LOST:
 	case MAP_SCCP_EV_RAN_DISC:
 	case MAP_SCCP_EV_USER_ABORT:
+	case MAP_SCCP_EV_CN_LINK_LOST:
 		/* RUA connection was terminated. First wait for the CC before releasing the SCCP conn. */
 		if (msg_has_l2_data(ranap_msg))
 			LOGPFSML(fi, LOGL_ERROR, "Connection not yet confirmed, cannot forward RANAP to CN\n");
@@ -379,6 +382,8 @@ static void map_sccp_connected_action(struct osmo_fsm_inst *fi, uint32_t event, 
 		 * Disconnect on the SCCP layer, ungracefully. */
 	case MAP_SCCP_EV_USER_ABORT:
 		/* The user is asking for disconnection, so there is no Iu Release in progress. Disconnect now. */
+	case MAP_SCCP_EV_CN_LINK_LOST:
+		/* The CN peer has sent a RANAP RESET, so the old link that this map ran on is lost */
 
 		/* There won't be any ranap_msg, but if a caller wants to dispatch a msg, forward it before
 		 * disconnecting. */
@@ -448,6 +453,7 @@ static void map_sccp_wait_rlsd_action(struct osmo_fsm_inst *fi, uint32_t event, 
 		return;
 
 	case MAP_SCCP_EV_USER_ABORT:
+	case MAP_SCCP_EV_CN_LINK_LOST:
 		/* Stop waiting for RLSD, send RLSD now. */
 		tx_sccp_rlsd(fi);
 		map_sccp_fsm_state_chg(MAP_SCCP_ST_DISCONNECTED);
@@ -525,6 +531,7 @@ static const struct osmo_fsm_state map_sccp_fsm_states[] = {
 			| S(MAP_SCCP_EV_RAN_LINK_LOST)
 			| S(MAP_SCCP_EV_RX_RELEASED)
 			| S(MAP_SCCP_EV_USER_ABORT)
+			| S(MAP_SCCP_EV_CN_LINK_LOST)
 			,
 		.out_state_mask = 0
 			| S(MAP_SCCP_ST_INIT)
@@ -542,6 +549,7 @@ static const struct osmo_fsm_state map_sccp_fsm_states[] = {
 			| S(MAP_SCCP_EV_RAN_LINK_LOST)
 			| S(MAP_SCCP_EV_RX_RELEASED)
 			| S(MAP_SCCP_EV_USER_ABORT)
+			| S(MAP_SCCP_EV_CN_LINK_LOST)
 			,
 		.out_state_mask = 0
 			| S(MAP_SCCP_ST_CONNECTED)
@@ -559,6 +567,7 @@ static const struct osmo_fsm_state map_sccp_fsm_states[] = {
 			| S(MAP_SCCP_EV_RX_RELEASED)
 			| S(MAP_SCCP_EV_RX_CONNECTION_CONFIRM)
 			| S(MAP_SCCP_EV_USER_ABORT)
+			| S(MAP_SCCP_EV_CN_LINK_LOST)
 			,
 		.out_state_mask = 0
 			| S(MAP_SCCP_ST_WAIT_RLSD)
@@ -577,6 +586,7 @@ static const struct osmo_fsm_state map_sccp_fsm_states[] = {
 			| S(MAP_SCCP_EV_RAN_LINK_LOST)
 			| S(MAP_SCCP_EV_RX_CONNECTION_CONFIRM)
 			| S(MAP_SCCP_EV_USER_ABORT)
+			| S(MAP_SCCP_EV_CN_LINK_LOST)
 			,
 		.out_state_mask = 0
 			| S(MAP_SCCP_ST_DISCONNECTED)
@@ -591,6 +601,7 @@ static const struct osmo_fsm_state map_sccp_fsm_states[] = {
 			| S(MAP_SCCP_EV_RAN_DISC)
 			| S(MAP_SCCP_EV_RAN_LINK_LOST)
 			| S(MAP_SCCP_EV_USER_ABORT)
+			| S(MAP_SCCP_EV_CN_LINK_LOST)
 			,
 		.onenter = map_sccp_disconnected_onenter,
 		.action = map_sccp_disconnected_action,

include/osmocom/hnbgw/context_map.h
@@ -66,6 +66,9 @@ enum map_sccp_fsm_event {
 	/* The human admin asks to drop the current SCCP connection, by telnet VTY 'apply sccp' in presence of SCCP
 	 * config changes. */
 	MAP_SCCP_EV_USER_ABORT,
+	/* The CN link can no longer work, for example a RANAP RESET was received from the cnlink that hosts this
+	 * context map. */
+	MAP_SCCP_EV_CN_LINK_LOST,
 };
 
 /* For context_map_get_state(), to combine the RUA and SCCP states, for VTY reporting only. */
@@ -186,6 +189,7 @@ int _map_sccp_dispatch(struct hnbgw_context_map *map, uint32_t event, struct msg
 
 bool map_rua_is_active(struct hnbgw_context_map *map);
 bool map_sccp_is_active(struct hnbgw_context_map *map);
+void context_map_cnlink_lost(struct hnbgw_context_map *map);
 void context_map_free(struct hnbgw_context_map *map);
 
 unsigned int msg_has_l2_data(const struct msgb *msg);

commit ca8fc60231bc1a1ff28aec36f11618276711a7e6

Fix MAP_SCCP_EV_CN_LINK_LOST never dispatched
Commit introducing event MAP_SCCP_EV_CN_LINK_LOST into the FSM actually
never dispatched it, and instead dispatched MAP_SCCP_EV_RAN_LINK_LOST,
probably a copy-paste typo.
At the same time, it becomes clear no param is passed to the FSM when
dispatching it, so update related code paths since it becomes clear no
ranap_msg is passed.

Regarding comment in map_sccp_connected_action() about sending a RANAP
RESET, that should be done previously through a
MAP_SCCP_EV_RX_DATA_INDICATION prior to dispatching the CN_LINK_LOST
event if needed, similar to how's done in the context_map_rua FSM.

Fixes: b1c0bb19e2bfbc48e05a89a5f09b476d1243b5f3
Change-Id: I3a37522da16d9a06764f58c2da9579b397d8a7f2

src/osmo-hnbgw/context_map.c
@@ -186,7 +186,7 @@ void context_map_hnb_released(struct hnbgw_context_map *map)
 
 void context_map_cnlink_lost(struct hnbgw_context_map *map)
 {
-	map_sccp_dispatch(map, MAP_SCCP_EV_RAN_LINK_LOST, NULL);
+	map_sccp_dispatch(map, MAP_SCCP_EV_CN_LINK_LOST, NULL);
 }
 
 void context_map_free(struct hnbgw_context_map *map)

src/osmo-hnbgw/context_map_sccp.c
@@ -196,7 +196,6 @@ static int handle_rx_sccp(struct osmo_fsm_inst *fi, struct msgb *ranap_msg)
 
 static void map_sccp_init_action(struct osmo_fsm_inst *fi, uint32_t event, void *data)
 {
-	struct hnbgw_context_map *map = fi->priv;
 	struct msgb *ranap_msg = NULL;
 
 	switch (event) {
@@ -212,10 +211,6 @@ static void map_sccp_init_action(struct osmo_fsm_inst *fi, uint32_t event, void 
 	case MAP_SCCP_EV_RAN_LINK_LOST:
 	case MAP_SCCP_EV_USER_ABORT:
 	case MAP_SCCP_EV_CN_LINK_LOST:
-		ranap_msg = data;
-		/* No CR has been sent yet, just go to disconnected state. */
-		if (msg_has_l2_data(ranap_msg))
-			LOG_MAP(map, DLSCCP, LOGL_ERROR, "SCCP not connected, cannot dispatch RANAP message\n");
 		map_sccp_fsm_state_chg(MAP_SCCP_ST_DISCONNECTED);
 		return;
 
@@ -259,10 +254,6 @@ static void map_sccp_wait_cc_action(struct osmo_fsm_inst *fi, uint32_t event, vo
 	case MAP_SCCP_EV_RAN_LINK_LOST:
 	case MAP_SCCP_EV_USER_ABORT:
 	case MAP_SCCP_EV_CN_LINK_LOST:
-		ranap_msg = data;
-		/* RUA connection was terminated. First wait for the CC before releasing the SCCP conn. */
-		if (msg_has_l2_data(ranap_msg))
-			LOGPFSML(fi, LOGL_ERROR, "Connection not yet confirmed, cannot forward RANAP to CN\n");
 		map->please_disconnect = true;
 		return;
 
@@ -341,12 +332,7 @@ static void map_sccp_connected_action(struct osmo_fsm_inst *fi, uint32_t event, 
 	case MAP_SCCP_EV_USER_ABORT:
 		/* The user is asking for disconnection, so there is no Iu Release in progress. Disconnect now. */
 	case MAP_SCCP_EV_CN_LINK_LOST:
-		ranap_msg = data;
 		/* The CN peer has sent a RANAP RESET, so the old link that this map ran on is lost */
-
-		/* There won't be any ranap_msg, but if a caller wants to dispatch a msg, forward it before
-		 * disconnecting. */
-		tx_sccp_df1(fi, ranap_msg);
 		tx_sccp_rlsd(fi);
 		map_sccp_fsm_state_chg(MAP_SCCP_ST_DISCONNECTED);
 		return;